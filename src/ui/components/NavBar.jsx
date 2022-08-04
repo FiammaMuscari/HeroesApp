61f8174d8998f9af8d5acbd9bec58b6d66d636ae
Beto <robledo.mdp@gmail.com>
2022-08-04 18:25:16 -0300

    first commit

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
             <Link 
                 className="navbar-brand" 
@@ -31,7 +31,7 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <NavLink 
                         className="nav-item nav-link" 
