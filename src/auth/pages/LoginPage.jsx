c38b4b57d749ad80127683c21caa0881eed1f9cf
Fiamma Muscari <fiammamuscari@gmail.com>
2022-08-15 22:25:54 -0300

    login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,29 +1,66 @@
-import React, { useContext } from "react";
+import React from "react";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import { useForm } from "../../hooks/useForm";
+import Typed from "react-typed";
+
 
 export const LoginPage = () => {
-
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
-  const { login } = useContext( AuthContext )
+
+  const { formState, onInputChange } = useForm({
+    username: "",
+  });
+  const { username } = formState;
 
   const onLogin = () => {
-
-    login('Fiamma Muscari');
-   
+    if (username.trim().length <= 1) return;
+    login(username);
     navigate("/", {
       replace: true,
     });
   };
 
-  return (
-    <div className="container mt-5">
-      <h1 className=" mb-3">LoginPage</h1>
-      <hr className=" mb-3"/>
 
-      <button className="btn btn-primary" onClick={onLogin}>
-        Login
-      </button>
-    </div>
+
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center flex-column vw-100 vh-100 bg-dark ">
+        <img
+          className=" rounded-circle" 
+          src="https://c.tenor.com/OrWIV_jmwE0AAAAM/heart-i-love-you.gif"
+          alt="gif"
+        />
+        <form onSubmit={() => onLogin()}>
+          <Typed
+          className="d-flex justify-content-center text-white"
+            strings={[
+              '"Bad Deadpool... Good Deadpool!"',
+              '"Say the magic words, Fat Gandalf."',
+              '"This is my most prized possession.."',
+              '"Captain Deadpool! No, just Deadpool."',
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+          />
+          <div className="d-grid gap-2 mt-3">
+            <input
+              style={{width:"40vw", borderRadius:"0.2em"}}
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={onInputChange}
+            ></input>
+            <button className="btn btn-outline-primary mt-3" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
